364d7585b6470ccac6a8ee322f3226d99a473400
pedro mendez <prmendez222@gmail.com>
2026-03-07 17:56:24 -0600

    ejercicio 1

--- ejercicio 1/Program.cs
+++ ejercicio 1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void saludar()
+{
+    Console.WriteLine("Hola, Bienvenido");
+}
+
+saludar();
