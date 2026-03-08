528e0d84093cf7659f1dc7fb266375957e9a1b47
pedro mendez <prmendez222@gmail.com>
2026-03-07 21:15:34 -0600

    ejercicio 17 terminado

---
+++ ejercicio 17/Program.cs
@@ -0,0 +1,22 @@
+﻿int MayorMenor(int a, int b)
+{
+    if (a>b) 
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+Console.Write("ingrese el primer numero: ");
+int num1=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("ingrese el segundo numero: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int mayor = MayorMenor(num1,num2);
+
+Console.WriteLine($"el numero mayor es: {mayor}");
