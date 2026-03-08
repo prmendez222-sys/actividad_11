e352a37f2211ca6c7aae9239ca0a59261b2867aa
pedro mendez <prmendez222@gmail.com>
2026-03-07 20:47:24 -0600

    ejercicio 12

---
+++ ejercicio 12/Program.cs
@@ -0,0 +1,16 @@
+﻿int suma(int a, int b)
+{
+    return a - b;
+}
+
+Console.Write("ingrese operando 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("ingrese operando 1: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int res = suma(num1, num2);
+
+Console.WriteLine($"el resultado de la resta es: {res}");
