211c691799772eac7684f300d3a4426f72cca301
pedro mendez <prmendez222@gmail.com>
2026-03-07 22:10:46 -0600

    ejercicio 23 terminado

---
+++ ejercicio 23/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumaN(int n)
+{
+    int suma=0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        suma += i;
+    }
+
+    return suma;
+}
+int suma = SumaN(5);
+
+Console.WriteLine($"la suma de los numeros: {suma} ");
