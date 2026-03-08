ce2997bd721aede655b11fc67979c73a79168ce0
pedro mendez <prmendez222@gmail.com>
2026-03-07 18:50:47 -0600

    ejercicio 7 terminado

---
+++ ejercicio 7/Program.cs
@@ -0,0 +1,11 @@
+﻿int MostrarDoble(int n)
+{
+    return n * 2;
+}
+
+Console.Write("ingrese un numero entero: ");
+
+int doble=MostrarDoble(int.Parse(Console.ReadLine()));
+
+Console.WriteLine();
+Console.WriteLine($"el doble del numero ingresado es: {doble}");
