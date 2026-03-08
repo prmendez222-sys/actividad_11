dd20b230e9996ff1e80b218bea47168e93dd1fc0
pedro mendez <prmendez222@gmail.com>
2026-03-07 21:37:15 -0600

    ejercicio 19 terminado

---
+++ ejercicio 19/Program.cs
@@ -0,0 +1,15 @@
+﻿bool EsMultiplo(int n, int m)
+{
+    if (n % m == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool multiplo = EsMultiplo(5,10);
+
+Console.WriteLine(multiplo);
