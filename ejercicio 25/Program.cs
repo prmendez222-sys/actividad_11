db92e456284a6c0d951706fb3aefa6f8becf46af
pedro mendez <prmendez222@gmail.com>
2026-03-07 22:27:30 -0600

    ejercicio 25 terminado

---
+++ ejercicio 25/Program.cs
@@ -0,0 +1,34 @@
+﻿bool Primo(int n)
+{
+    bool esPrimo = true;
+
+    if (n == 2)
+    {
+        esPrimo = true; 
+    }
+
+    if(n==1)
+        esPrimo= false;
+
+    for(int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+
+    return esPrimo;
+}
+
+bool primo = Primo(4);
+
+if (primo == true)
+{
+    Console.WriteLine("el numero es primo");
+}
+else
+{
+    Console.WriteLine("el numero no es primo");
+}
