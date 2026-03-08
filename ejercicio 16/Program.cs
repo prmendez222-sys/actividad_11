e7e5f9028f2916d23091b1af5d776130caaa1852
pedro mendez <prmendez222@gmail.com>
2026-03-07 21:06:39 -0600

    ejercicio 16 completo

---
+++ ejercicio 16/Program.cs
@@ -0,0 +1,22 @@
+﻿bool EsPositivo(int n)
+{
+    if (n >0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool par = EsPositivo(-10);
+
+if (par == true)
+{
+    Console.WriteLine("el numero es positivo");
+}
+else
+{
+    Console.WriteLine("el numero es negativo");
+}
