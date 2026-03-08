c0756b13a024a70cbba344caa316ce8936a65dfd
pedro mendez <prmendez222@gmail.com>
2026-03-07 21:02:48 -0600

    ejercicio 15 completo

---
+++ ejercicio 15/Program.cs
@@ -0,0 +1,22 @@
+﻿bool EsPar(int n)
+{
+    if (n % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool par = EsPar(10);
+
+if (par == true)
+{
+    Console.WriteLine("el numero es Par");
+}
+else
+{
+    Console.WriteLine("el numero es impar");
+}
