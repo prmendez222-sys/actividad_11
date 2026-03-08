0617b2cc5ca2c5b8b28351eb0fd247a0914c1214
pedro mendez <prmendez222@gmail.com>
2026-03-07 20:00:17 -0600

    ejercicio 8 terminado

---
+++ ejercicio 8/Program.cs
@@ -0,0 +1,9 @@
+﻿int CuadradoNumero(int Cuad)
+{
+    return Cuad*Cuad;
+}
+
+Console.Write("ingrese un numero: ");
+int cuadrado = CuadradoNumero(int.Parse(Console.ReadLine()));
+
+Console.WriteLine($"el cuadrado del numero es: {cuadrado}");
