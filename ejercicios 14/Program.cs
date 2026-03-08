c4b7287e221a736ead175422e61f0505800295c9
pedro mendez <prmendez222@gmail.com>
2026-03-07 20:57:02 -0600

    ejercicio 14 completo

---
+++ ejercicios 14/Program.cs
@@ -0,0 +1,22 @@
+﻿int Divicion(int a, int b)
+{
+    return a / b;
+}
+
+Console.Write("ingrese operando 1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("ingrese operando 1: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (num2 != 0)
+{
+    int res=Divicion(num1, num2);
+    Console.WriteLine($"el resultado de la divicion es: {res}");
+}
+else
+{
+    Console.WriteLine("no se puede dividir en cero");
+}
