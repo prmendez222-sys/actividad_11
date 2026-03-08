0f16cb6b2a90d326b944d65ba336e595efe0624d
pedro mendez <prmendez222@gmail.com>
2026-03-07 22:17:49 -0600

    ejercicio 24 terminado

---
+++ ejercicio 24/Program.cs
@@ -0,0 +1,14 @@
+﻿int Factorial(int n)
+{
+    int factorial=1;
+    for(int i = 1; i <= n; i++)
+    {
+        factorial *= i;
+    }
+
+    return factorial;
+}
+
+int factrial=Factorial(5);
+
+Console.WriteLine("el factorial del numero es: " + factrial);
