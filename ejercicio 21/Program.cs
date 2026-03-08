9561c2c9bc69fdeeee045744258b50471e517b97
pedro mendez <prmendez222@gmail.com>
2026-03-07 21:57:07 -0600

    ejercicio 21 terminado

---
+++ ejercicio 21/Program.cs
@@ -0,0 +1,17 @@
+﻿int contVocales(string texto)
+{
+    texto = texto.ToLower();
+    int cont = 0;
+
+    foreach (char c in texto)
+    {
+        if (c == 'a' || c =='e' ||  c =='i' ||  c =='o' || c =='u') 
+            cont++;
+    }
+
+    return cont;
+}
+
+int contV = contVocales("hola");
+
+Console.WriteLine($"el texto tiene {contV} vocales");
